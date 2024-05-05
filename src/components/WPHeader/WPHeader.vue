584e2695a1657354b127a7110487c40dc26d2ba7
Sai Shashaank V V <saishashaankvv@gmail.com>
2024-05-05 19:26:45 +0100

    header skeleton complete. unit tests pending

---
+++ src/components/WPHeader/WPHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="wp-header">
+    <WPNavLogo />
+    <WPNavigation />
+  </header>
+</template>
+
+<script setup lang="ts">
+import { WPNavLogo, WPNavigation } from '@/components'
+</script>
+
+<style scoped>
+.wp-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
