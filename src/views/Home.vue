33b193db1447685d7776205e36253da91b65f8ac
Sai Shashaank V V <saishashaankvv@gmail.com>
2024-05-13 09:16:26 +0100

    initial hero commit

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="wp-hero">
+    <div class="wp-hero-img"></div>
+    <div class="wp-hero-content"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
